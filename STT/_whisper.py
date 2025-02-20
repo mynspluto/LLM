462de7e77464f06700e8183198dfb78e79d420a5
mynspluto <mynspluto@gmail.com>
2025-02-20 14:58:54 +0900

    whisper 사용시 음원 상대 경로

--- STT/_whisper.py
+++ STT/_whisper.py
@@ -1,6 +1,11 @@
 import whisper
+import os
+
+# 맥 음성 메모로 만든 파일
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, "../audio.m4a")
 
 # https://github.com/openai/whisper
 model = whisper.load_model("medium")
-result = model.transcribe("audio.m4a")
+result = model.transcribe(file_path)
 print(result["text"])
